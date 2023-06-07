259b09481f190fc825008b6840a9f9a8805dfa2f
Laura Untner <83955990+laurauntner@users.noreply.github.com>
2023-06-07 12:05:46 +0200

    Update fetch-data.sh

--- fetch-data.sh
+++ fetch-data.sh
@@ -4,7 +4,6 @@
 
 rm -rf editions
 wget https://github.com/arthur-schnitzler/schnitzler-briefe-data/archive/refs/heads/main.zip
-rm -rf editions
 unzip main.zip
 
 mv schnitzler-briefe-data-main/data/editions editions/
