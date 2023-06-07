cb2781786bea727c2f23e21b6ca0f643b3b9ea0a
Laura Untner <laurauntner@Lauras-MacBook-Pro-5.local>
2023-06-07 12:45:50 +0200

    include error messages for debugging

--- remove-empty-namespace-declarations.py
+++ remove-empty-namespace-declarations.py
@@ -3,15 +3,26 @@ import os
 directory = "./OEBPS/texts"
 
 def parse_xhtml_files(directory):
+    xhtml_files_found = False  # Flag to track if any XHTML files are found
+    
     for filename in os.listdir(directory):
         if filename.endswith(".xhtml"):
+            xhtml_files_found = True  # Set the flag to True
             filepath = os.path.join(directory, filename)
-            with open(filepath, "r") as file:
-                content = file.read()
             
-            updated_content = content.replace(' xmlns=""', '')
+            try:
+                with open(filepath, "r") as file:
+                    content = file.read()
 
-            with open(filepath, "w") as file:
-                file.write(updated_content)
+                updated_content = content.replace(' xmlns=""', '')
+
+                with open(filepath, "w") as file:
+                    file.write(updated_content)
+                    
+            except Exception as e:
+                print(f"Error processing file '{filepath}': {str(e)}")
+
+    if not xhtml_files_found:
+        print("No XHTML files found in the specified directory.")
 
 parse_xhtml_files(directory)
--- remove-tei-files.py
+++ remove-tei-files.py
@@ -3,10 +3,12 @@ import re
 
 def delete_files_with_tei_tag(directory):
     tei_pattern = re.compile(r'<TEI\b[^>]*>.*?</TEI>', re.DOTALL | re.IGNORECASE)
+    xhtml_files_found = False  # Flag to track if any XHTML files are found
     
     for root, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith('.xhtml'):
+                xhtml_files_found = True  # Set the flag to True
                 file_path = os.path.join(root, file)
                 try:
                     with open(file_path, 'r', encoding='utf-8') as f:
@@ -17,6 +19,9 @@ def delete_files_with_tei_tag(directory):
                         print(f"Deleted file: {file_path}")
                 except IOError:
                     print(f"Error reading file: {file_path}")
+    
+    if not xhtml_files_found:
+        print("No XHTML files found in the specified directory.")
 
 def contains_tei_tag(content, tei_pattern):
     return bool(tei_pattern.search(content))
