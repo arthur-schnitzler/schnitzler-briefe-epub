5305bbf8331d032c6079725de55d1c8b2a7306cf
Laura Untner <laurauntner@Lauras-MacBook-Pro-5.local>
2023-06-07 14:39:27 +0200

    debugging

--- remove-tei-files.py
+++ remove-tei-files.py
@@ -6,36 +6,37 @@ def delete_files_with_tei_tag(directory):
     xhtml_files_found = False  # Flag to track if any XHTML files are found
     removed_files = []  # List to store the names of the removed files
     namespaces = set()  # Set to store the distinct namespaces
-    
+
     def contains_tei_tag(content, tei_pattern):
         return bool(tei_pattern.search(content))
-    
+
     for root, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith('.xhtml'):
                 xhtml_files_found = True  # Set the flag to True
                 file_path = os.path.normpath(os.path.join(root, file))
+                print(f"Processing file: {file_path}")
                 try:
                     with open(file_path, 'r', encoding='utf-8') as f:
                         content = f.read()
-                    
+
                     if contains_tei_tag(content, tei_pattern):
                         os.remove(file_path)
                         removed_files.append(file)  # Add the filename to the removed files list
-                        
+
                         # Extract the distinct namespaces from the xmlns declarations
                         matches = re.findall(r'xmlns:(\w+)=', content)
                         namespaces.update(matches)
                 except IOError:
                     print(f"Error reading file: {file_path}")
-    
+
     if not xhtml_files_found:
         print("No XHTML files found in the specified directory.")
     else:
         print("Removed files:")
         for file in removed_files:
             print(file)
-        
+
         print("Distinct namespaces:")
         for namespace in namespaces:
             print(namespace)
