6a58014ae409a9fe25367b185bee383234b18c57
Laura Untner <laurauntner@Lauras-MacBook-Pro-5.local>
2023-06-09 09:10:54 +0200

    das brauch ma ned

--- shellscripts/script.sh
+++ shellscripts/script.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# hacky way to make shure the script 
+# hacky way to make sure the script 
 # gets always run from parent-dir 
 # so relative paths get resolved the right way
 script_location_dir="${BASH_SOURCE[0]}"
 cd $script_location_dir && cd ..
-# run scripts
-./shellscripts/dl_fundament.sh
+# run script
 ./shellscripts/dl_saxon.sh
